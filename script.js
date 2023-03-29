780c845e9d83a5c9432afe899e76216ec5eab19f
Mamta <120008345+Mamta13kunal@users.noreply.github.com>
2023-03-29 23:03:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+	var isPalindrome = function(s) {
+    var strippedString = s.replace(/\W/g, '');
+    var reversedString = strippedString.split('').reverse().join('');
+    
+    return strippedString.toLowerCase() == reversedString.toLowerCase();
+};
 
 }
 module.exports = palindrome
